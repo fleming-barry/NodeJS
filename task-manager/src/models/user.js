9f3f69f7c5885ae5e73741a2a9a43a32dd72ef6f
Barry Fleming <b.fleming@trustetc.com>
2019-07-02 14:05:43 -0400

    Section 92 completed

---
+++ task-manager/src/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+        validate(val) {
+            if (!validator.isEmail(val)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(val) {
+            if (val.toLowerCase().includes('password')) {
+                throw new Error('Can not contain the word password')
+            }
+        }
+
+    }
+})
+
+module.exports = User
