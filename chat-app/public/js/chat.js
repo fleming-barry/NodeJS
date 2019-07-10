899dce50cc0efe0a17cbf55f0c6155a08edd976d
Barry Fleming <b.fleming@trustetc.com>
2019-07-10 15:40:22 -0400

    Part 155 completed

---
+++ chat-app/public/js/chat.js
@@ -0,0 +1,10 @@
+const socket = io()
+
+socket.on('countUpdated', (count) => { 
+    console.log('The count has been updated', count)
+})
+
+document.querySelector('#increment').addEventListener('click', () => { 
+    console.log('Clicked the button')
+    socket.emit('increment')
+})
